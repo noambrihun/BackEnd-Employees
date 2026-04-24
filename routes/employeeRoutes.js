42df874620657e617837bb05b1991707d2587932
noambrihun <noambrihun1@gmail.com>
2026-04-24 16:22:27 +0300

    completed backend CRUD employee manager

---
+++ routes/employeeRoutes.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+
+const Employee = require("../models/Employee");
+const { models } = require("mongoose");
+
+router.get("/", async (req, res) => {
+  try {
+    const employees = await Employee.find();
+    res.json(employees);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const newEmployee = await Employee.create(req.body);
+    res.status(201).json(newEmployee);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    await Employee.findByIdAndDelete(req.params.id);
+    res.json({ message: "Employee deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.patch("/:id", async (req, res) => {
+  try {
+    const updatedEmployee = await Employee.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.json(updatedEmployee);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
+module.exports = router;
